0f26695d7139f1a8a94d1620b0242cc453f74070
snowcat-jp <snowcat.dev.jp@gmail.com>
2021-05-01 23:34:04 +0900

    apiエラーのJSON化

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -38,4 +38,69 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $throwable)
+    {
+        if ($request->is('api/*')) {
+            return $this->apiRender($request, $throwable);
+        } else {
+            return parent::render($request, $throwable);
+        }
+    }
+    
+    private function apiRender($request, Throwable $throwable)
+    {
+        $exception = $this->prepareException($throwable);
+
+        return $this->customApiResponse($exception);
+    }
+
+    private function customApiResponse($exception)
+    {
+        $statusCode = 500;
+        if (method_exists($exception, 'getStatusCode')) {
+            $statusCode = $exception->getStatusCode();
+        }
+        $responses = [
+            'code' => $statusCode
+        ];
+        switch ($statusCode) {
+            case 401:
+                $responses['message'] = 'Unauthorized';
+                $responses['user_message'] = '認証できませんでした。ログインしてください。';
+                // $responses['user_message'] = __('exception.E401');
+                break;
+            case 403:
+                $responses['message'] = 'Forbidden';
+                $responses['user_message'] = '指定されたリクエストは許可されていません。';
+                // $responses['user_message'] = __('exception.E403');
+                break;
+            case 404:
+                $responses['message'] = 'Not Found';
+                $responses['user_message'] = '対象のデータは見つかりませんでした。';
+                // $responses['user_message'] = __('exception.E404');
+                break;
+            case 405:
+                $responses['message'] = 'Method Not Allowed';
+                $responses['user_message'] = '指定されたHTTPメソッドは対応していません。';
+                // $responses['user_message'] = __('exception.E405');
+                break;
+            case 500:
+                $responses['message'] = 'Internal Server Error';
+                $responses['user_message'] = 'エラーが発生しました。システムお問い合わせフォームからお問い合わせください。';
+                // $responses['user_message'] = __('exception.E500');
+                break;
+            case 503:
+                $responses['message'] = 'Service Temporarily Unavailable';
+                $responses['user_message'] = 'アクセスが集中しているためエラーが発生しました。';
+                // $responses['user_message'] = __('exception.E503');
+                break;
+            default:
+                $responses['message'] = 'Internal Server Error';
+                $responses['user_message'] = 'エラーが発生しました。システムお問い合わせフォームからお問い合わせください。';
+                // $responses['user_message'] = __('exception.default');
+                break;
+        }
+        return response()->json(['error' => $responses], $statusCode);
+    }
 }
---
+++ backend/resources/lang/ja/exception.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'E401' => '認証できませんでした。ログインしてください。',
+    'E403' => '指定されたリクエストは許可されていません。',
+    'E404' => '対象のデータは見つかりませんでした。',
+    'E405' => '指定されたHTTPメソッドは対応していません。',
+    'E500' => 'エラーが発生しました。システムお問い合わせフォームからお問い合わせください。',
+    'E503' => 'アクセスが集中しているためエラーが発生しました。',
+    'default' => 'エラーが発生しました。システムお問い合わせフォームからお問い合わせください。',
+];
