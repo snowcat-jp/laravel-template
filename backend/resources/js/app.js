06e38c9bf2f87673e129547c977388708e56be5c
snowcat-jp <snowcat.dev.jp@gmail.com>
2021-05-03 02:27:32 +0900

    ページ追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -23,6 +23,9 @@ window.Vue = require('vue').default;
 
 import router from "./routes/router";;
 
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -31,5 +34,8 @@ import router from "./routes/router";;
 
 const app = new Vue({
     el: '#app',
-    router: router
+    router: router,
+    vuetify: new Vuetify({
+        iconfont: 'mdi',
+    }),
 });
--- backend/resources/js/components/login.vue
+++ backend/resources/js/components/login.vue
@@ -3,10 +3,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">login</div>
 
                     <div class="card-body">
-                        I'm an example component.
                     </div>
                 </div>
             </div>
