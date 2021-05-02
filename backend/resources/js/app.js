cf456e9b7e68a012e798e3a0bcbc920dcaf9f1c9
snowcat-jp <snowcat.dev.jp@gmail.com>
2021-05-03 01:37:55 +0900

    vue-router対応

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+require("./bootstrap");
+ 
+window.Vue = require("vue");
+ 
+import Vue from "vue";
+import router from "./routes/router";
+ 
+const app = new Vue({
+    el: "#app",
+    router: router
+});
